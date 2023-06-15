3657c92c8c15f4159c66fe8bbc82963ebeaf9fe0
mattstyles <matt@personalurban.com>
2023-06-15 23:18:49 +0100

    rename :house: interop to either tests

--- src/types/result.ts
+++
@@ -1,9 +0,0 @@
-export interface Ok<A> {
-  readonly _tag: 'ok'
-  readonly value: A
-}
-export interface Err<E> {
-  readonly _tag: 'err'
-  readonly err: E
-}
-export type Result<A, E = unknown> = Ok<A> | Err<E>
